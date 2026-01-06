3bba70a18ffb64047b137c37af015a2d5051b97f
daurenk2004-star <daurenk2004@gmail.com>
2026-01-07 04:35:42 +0500

    Update GlobalExceptionHandler.java
    asdada

--- src/main/java/ru/codekitchen/controller/exception/GlobalExceptionHandler.java
+++ src/main/java/ru/codekitchen/controller/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package ru.codekitchen.controller.exception;
 
-import org.springframework.boot.webmvc.error.ErrorController;
+//import org.springframework.boot.webmvc.error.ErrorController;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
